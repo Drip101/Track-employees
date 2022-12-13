3436d360a64e26f61e0f4e58a99810b7f0fa82bf
Maddrick Long <110777599+Drip101@users.noreply.github.com>
2022-12-13 12:57:19 -0500

    create files and dbs

---
+++ db/seeds.sql
@@ -0,0 +1,13 @@
+INSERT INTO department (department_name)
+VALUES ("HR Department"),
+       ("Enterprise Department"),
+       ("Recruiting Department"),
+       ("Finance Department"),
+       ("Helpdesk Department");
+
+INSERT INTO role (title, salary)
+VALUES ("Customer Service" , 40,000),
+       ("IT", 60,000),
+       ("Enterprise Technology representative", 120,000),
+       ("Finance Representative", 80,000);
+-- INSERT INTO employee (movie_name)
